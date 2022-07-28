43710ea575becd5c2db790cdca1ce5b339918ab2
Vpkarthik27 <110039732+Vpkarthik27@users.noreply.github.com>
2022-07-28 22:45:00 +0530

    Update main-s3.tf

--- main-s3.tf
+++ main-s3.tf
@@ -1,7 +1,8 @@
 terraform {
   required_version = ">0.14.7"
   backend "s3" {
-      bucket = "terraform-state-keeper12/ecs/prod/terraform.tfstate"
+  bucket = "terraform-state-keeper12"
   region = "ap-south-1"
-  }
+  key = "pt-tf/dev/terraform.tfstate"
+    }
   }
