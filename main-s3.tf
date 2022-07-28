c09da68dbf5a1ec9a7a3d4e59e15c3ec214153f8
Vpkarthik27 <110039732+Vpkarthik27@users.noreply.github.com>
2022-07-28 22:47:54 +0530

    Update main-s3.tf

--- main-s3.tf
+++ main-s3.tf
@@ -6,3 +6,13 @@ terraform {
   key = "pt-tf/dev/terraform.tfstate"
     }
   }
+resource "aws_instance" "ec2_linux"{
+  ami = "ami-08df646e18b182346"
+  region = "ap-south-1"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1b"
+  
+  tags= {
+     Name = "instancename"
+       }
+}
